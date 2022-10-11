54af7c07c9a7fdabf6b709c15e6f7a09b197abf5
christopher-k-c <chriscareyk@gmail.com>
2022-10-11 15:18:17 +0100

    Last Grammar and Spelling Update

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -17,17 +17,15 @@ export default function About() {
                 <br />
                 <p>
                 
-                The Software Engineering Immersive course with General Assembly has exposed me to industry standard
-                methodologies, technologies and more. Working in groups as well as on our own, we participated in daily 
-                stand ups, lectures and project presentations.
+                The Software Engineering Immersive course with General Assembly has exposed me to industry-standard methodologies, technologies and more. Working in groups and on our own, we participated in daily stand-ups, lectures and project presentations.
                 
                 </p>
                 <br />
                 <ul>
-                    <li>Developed agile work flows within our group projects</li>
-                    <li>Developed a confidence in HTML, CSS and JAVASCRIPT</li>
-                    <li>Most morning sessions would begin with a warm up challenge</li>
-                    <li>Built four projects, ranging from static sites to dynamic applications</li>
+                    <li>I developed agile workflows within our group projects</li>
+                    <li>I developed proficiency in HTML, CSS and JAVASCRIPT</li>
+                    <li>Most morning sessions would begin with a warm-up challenge on Code Wars</li>
+                    <li>I built four projects, ranging from static sites to dynamic applications</li>
                     <li>Responsible for handing in our homework on time</li>
                     <li>Problem solved in groups via pair programming</li>
                 </ul>
@@ -46,30 +44,27 @@ export default function About() {
                 <p className='date'>May 2015 - May 2022</p>
                 <br />
                 <p>
-                During my time at Jojo, I was promoted to Senior Retoucher, 
-                where I built an efficient team and implemented an agile inspired workflow. 
-                Regularly communicated at all levels across the company, to ensure my team 
-                delivered on short and long term deadlines.                
+                During my time at Jojo, I was promoted to Senior Retoucher, where I built an efficient team and implemented an agile-inspired workflow. Regularly communicated at all levels across the company to ensure my team delivered on short and long-term deadlines.     
                 </p>
 <br />
                 <ul>
                     <li>Processing, editing, compositing and grading images using Photoshop and Capture One.</li>
                     <li>Automated Photoshop processes.</li>
-                    <li>Working with Buying and Design on image amendments and colour correction.</li>
+                    <li>I worked with Buying and Design on image amendments and colour correction.</li>
                     <br />
                     <li>Used Exiftool and Crontab to build a CSV database of image metadata.</li>
-                    <li>Built a file finding application using Python, Tkinter, and OS modules.</li>
-                    <li>Transferred files using Rsync and Fillezilla.</li>
+                    <li>Built a file-finding application using Python, Tkinter, and OS modules.</li>
+                    <li>Transferred files using Rsync and Filezilla.</li>
                     <br />
                     <li>Working in Illustrator and Indesign to collaborate with Digital and Print Design on campaigns and promotional material.</li>
                     <li>Working with the Ecommerce team to ensure consistency of product and campaign images.</li>
-                    <li>Using Magento One to upload simple and configurable products. Flushing webpage caches.</li>
+                    <li>I used Magento One to upload simple and configurable products. Flushing webpage caches.</li>
                     <br />
-                    <li>Managed a team of 2 retouchers and 1 photographer. This included, but was not limited to, interviewing, training, personal development reviews.</li>
-                    <li>Managed the production process from beginning to end, delegating to the team, and ensuring KPIs and deadlines are met.</li>
-                    <li>Coordinated with freelancers and outsourcing agencies.</li>
-                    <li>Implemented trello into my teams workflow</li>
-                    <li>Utilised Unito to connect multiple trello boards</li>
+                    <li>I managed a team of two retouchers and one photographer. Including, but not limited to, interviewing, training, and personal development reviews.</li>
+                    <li>I managed the production process from beginning to end, delegating to the team and ensuring KPIs and deadlines were met.</li>
+                    <li>I coordinated with freelancers and outsourcing agencies.</li>
+                    <li>I Implemented Trello into my teams' workflow.</li>
+                    <li>Utilised Unito to connect multiple Trello boards.</li>
                 </ul>
             </div>
 
